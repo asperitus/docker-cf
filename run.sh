5d738cb88299fc0c8867ee172d3b1373c77216b7
Qiang Li <liqiang@gmail.com>
2018-01-07 20:40:20 -0800

    update run.sh

--- run.sh
+++ run.sh
@@ -12,18 +12,44 @@ set -x
 
 #find local ip for X server
 #required utils
+#net-tools (ifconfig)
 printf "ifconfig \n awk \n xset \n xhost \n" | xargs -n1 -I{} sh -c 'which {} || exit 255'; if [ $? -ne 0 ]; then
     exit 1
 fi
+##
+#Ubuntu: socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CONNECT:/tmp/.X11-unix/X0
 #
+#Mac OSX: socat TCP-LISTEN:6000,reuseaddr,fork UNIX-CLIENT:\"$DISPLAY\"
+##
 ipaddr=$(ifconfig | grep 'inet ' | grep -e '10\.' -e '172\.' -e '192\.'| awk '{$1=$1; print}'|cut -d' ' -f2 | cut -d: -f2 | xargs -n1 -I{} bash -c 'export DISPLAY={}:0; xset q 2>&1 > /dev/null && echo {} && exit 255;' 2> /dev/null)
 
 export DISPLAY=${ipaddr}:0
 xhost + $ipaddr
 
-##
+#
+unix_name=$(uname -a)
+case "${unix_name}" in
+    *Microsoft* )
+        echo "Windows 10 setup ..."
+        export DHNT_BASE="/c/Users/liqiang/.dhnt"
+    ;;
+    *Darwin* )
+        echo "Mac OSX setup ..."
+        export DHNT_BASE=~/.dhnt
+    ;;
+    *Ubuntu* )
+        echo "Ubuntu setup ..."
+        export DHNT_BASE=~/.dhnt
+    ;;
+    *)
+        echo "Default setup ..."
+        export DHNT_BASE=~/.dhnt
+esac
+
+#
 volume=""
 [ -d "/private/tmp" ] && volume="$volume -v /private/tmp:/private/tmp"
+[ -d "/tmp/.X11-unix" ] && volume="$volume -v /tmp/.X11-unix:/tmp/.X11-unix"
 
 [ ! -z "${DHNT_VCAP_HOME}" ] && volume="$volume -v ${DHNT_VCAP_HOME}:/home/vcap"
 [ ! -z "${GOPATH}" ] && volume="$volume -v ${GOPATH}:/home/vcap/go"
